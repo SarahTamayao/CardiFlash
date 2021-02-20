819f924af4be3de0ddee7ec1e030bf2d2282b3a6
Ponette <ponette@umich.edu>
2021-02-20 16:40:48 -0500

    Simple flashcards done

--- CardiFlash/ViewController.swift
+++ CardiFlash/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var front_label: UILabel!
+    @IBOutlet weak var back_label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+//        front_label.isHidden = true
     }
 
-
+    @IBAction func did_tap_on_flashcard(_ sender: Any) {
+        //
+        front_label.isHidden = true
+    }
+    
 }
 
